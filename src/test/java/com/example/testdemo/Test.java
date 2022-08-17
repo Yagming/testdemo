c069b5e2bcb6d33e2a7cca2936fce6f0df675475
lbm <lbm@atguigu.com>
2022-08-17 19:15:59 +0800

    first commit from idea

---
+++ src/test/java/com/example/testdemo/Test.java
@@ -0,0 +1,56 @@
+package com.example.testdemo;
+
+
+import java.util.*;
+
+public class Test {
+
+    @org.junit.jupiter.api.Test
+    public void test01() {
+
+        Set set = new LinkedHashSet();
+        set.add(1);
+        set.add(2);
+        set.add(1);
+        System.out.println(set);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void test02(){
+
+
+        Map map = new HashMap();
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+        map.put("key1", "newValue");
+        System.out.println(map);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void test03(){
+
+        Hashtable hashtable = new Hashtable();
+        for (int i = 1; i <= 9; i++) {
+            hashtable.put(i, i);
+        }
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void test04(){
+
+        TreeSet treeSet = new TreeSet(new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return ((String)o1).compareTo(((String)o2));
+            }
+        });
+
+
+    }
+
+
+
+}
